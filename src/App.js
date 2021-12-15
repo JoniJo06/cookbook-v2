daa8bdbc39da079a094c32dee6b6bdaa993a2f16
555ekstase <martinbibow@gmx.de>
2021-12-15 17:08:48 +0100

    Mett!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 /** @format */
 
-import NavigationDefinition from './Components/NavigationDefinition';
-import './index.css';
-import { useState } from 'react';
-import { Button } from '@mui/material';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
+import NavigationDefinition from "./Components/NavigationDefinition";
+import "./index.css";
+
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Button, TextField } from "@mui/material";
 
 /* *** *** *** *** *** *** *** ***\
 |* In kompetenter Zusammenarbeit *|
@@ -16,19 +19,68 @@ import Footer from './Components/Footer';
 \*** *** *** *** *** *** *** *** */
 
 function App() {
-	const [adminLogin, setAdminLogin] = useState();
-	return (
-		<div className='App'>
-			<header>
-				<Header />
-			</header>
-			{!adminLogin ? <Button>Log in</Button> : <Button>Log out</Button>}
-			<NavigationDefinition adminLogin={adminLogin} />
-			<footer>
-				<Footer />
-			</footer>
-		</div>
-	);
+  const [adminLogin, setAdminLogin] = useState(false);
+  const [user, setUser] = useState();
+
+  useEffect(() => {}, []);
+
+  const handleLogIn = () => {};
+  const handleLogOut = () => {
+    setAdminLogin(false);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const item = e.target;
+    // console.log(item.children[1].value)
+    await axios
+      .get(`http://localhost:4001/admins/${item.children[1].value}`)
+      .then((res) => setUser(res.data))
+      .catch((err) => console.error(err));
+    if (item.children[3].value === user.password) {
+			setAdminLogin(true);
+			item.style.opacity = 0;
+			item.style.pointerEvents = 'none';
+    } else {
+      alert("username oder passwort falsch");
+    }
+  };
+  return (
+    <div className="App">
+      <header>
+        <Header />
+      </header>
+      <div id="loginContainer">
+        {!adminLogin ? (
+          <Button variant="contained" onClick={handleLogIn}>
+            Log in
+          </Button>
+        ) : (
+          <Button variant='contained' onClick={handleLogOut}>Log out</Button>
+        )}
+        {adminLogin && (
+          <TextField
+            id="standard-read-only-input"
+            value=""
+            InputProps={{
+              readOnly: true,
+            }}
+            variant="standard"
+          />
+        )}{" "}
+        <form style={{ opacity: "1" }} id="logInForm" onSubmit={handleSubmit}>
+          <lable>username</lable>
+          <input required type="text" />
+          <lable>password</lable>
+          <input required type="text" />
+          <input value="login" type="submit" />
+        </form>
+      </div>
+      <NavigationDefinition />
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 }
 
 export default App;
--- src/Components/NavigationDefinition.jsx
+++ src/Components/NavigationDefinition.jsx
@@ -1,20 +1,23 @@
 /** @format */
 
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './HomePage';
-import Recipes from './Recipes';
-import CreateRecipe from './CreateRecipe';
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./HomePage";
+import Recipes from "./Recipes";
+import CreateRecipe from "./CreateRecipe";
+
 
 const NavigationDefinition = () => {
-	return (
-		<div>
-			<Routes>
-				<Route path='/' element={<HomePage />} />
-				<Route path='recipes/:id' element={<Recipes />} />
-				<Route path='create-recipe' element={<CreateRecipe />} />
-			</Routes>
-		</div>
-	);
+  
+  return (
+    <div>
+      
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="recipes/:id" element={<Recipes />} />
+        <Route path="create-recipe" element={<CreateRecipe />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default NavigationDefinition;
