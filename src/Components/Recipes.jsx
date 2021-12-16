1d75b797d56c92cda2cf0af2fd9b93d0249b3da8
JoniJo06 <jonasjuergens6@gmail.com>
2021-12-16 11:21:36 +0100

    fix delete function

--- src/Components/Recipes.jsx
+++ src/Components/Recipes.jsx
@@ -1,79 +1,73 @@
 /** @format */
 
-import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
-import { Container, Button } from "@mui/material";
-import axios from "axios";
-import "./../index.css";
+import { useNavigate, useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Container, Button } from '@mui/material';
+import axios from 'axios';
+import './../index.css';
 
 const Recipes = ({ adminLogin }) => {
-  const { id } = useParams();
-  const [recipe, setRecipe] = useState([]);
-  // const [ingredients, setIngredients] = useState();
-  const [isLoading, setIsLoading] = useState(true);
+	const { id } = useParams();
+	const [recipe, setRecipe] = useState([]);
+	// const [ingredients, setIngredients] = useState();
+	const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      await  axios
-        .get(`https://kochbuch-backend.herokuapp.com/recipes/${id}`)
-        .then((res) => setRecipe(res.data))
-        .catch((err) => console.error(err));
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const fetchData = async () => {
+			await axios
+				.get(`https://kochbuch-backend.herokuapp.com/recipes/${id}`)
+				.then((res) => setRecipe(res.data))
+				.catch((err) => console.error(err));
 			setIsLoading(false);
-    };
-    fetchData();
-  }, [id]);
+		};
+		fetchData();
+	}, [id]);
 
-  const handleDelete = () => {
-    const input = prompt("wirklich löschen?\n \n dann bitte DELETE eingeben!");
-    if (input === "DELETE") {
-      axios
-        .delete(`https://kochbuch-backend.herokuapp.com/recipes/${id}`)
-        .then((res) => alert("rezept wurde gelöscht"))
-        .catch((err) => console.error(err));
-    }
-  };
+	const handleDelete = async () => {
+		const input = prompt('wirklich löschen?\n \n dann bitte DELETE eingeben!');
+		console.log(id);
+		if (input === 'DELETE') {
+			console.log(input);
+			await axios
+				.delete(`http://localhost:4001/recipes/${id}`)
+				.then((res) => alert('rezept wurde gelöscht'))
+				.catch((err) => console.error(err));
+			navigate('/');
+		}
+	};
 
-  return (
-    <>
-      {!isLoading ? (
-        <Container maxWidth="100%" className="Recipes">
-          <h1 className="recipeHeader">{recipe.title}</h1>
-          <div className="imageContainer">
-            <img className="meal" src={recipe.picture_one} alt="#" />
-            {recipe.picture_two && (
-              <img className="meal" src={recipe.picture_two} alt="#" />
-            )}
-          </div>
-          <h3>Zutaten</h3>
-          {recipe.ingredients.split("\n").map((el, i) => {
-            // console.log(el)
-            return <p key={i}>{el}</p>;
-          })}
+	return (
+		<>
+			{!isLoading ? (
+				<Container maxWidth='100%' className='Recipes'>
+					<h1 className='recipeHeader'>{recipe.title}</h1>
+					<div className='imageContainer'>
+						<img className='meal' src={recipe.picture_one} alt='#' />
+						{recipe.picture_two && <img className='meal' src={recipe.picture_two} alt='#' />}
+					</div>
+					<h3>Zutaten</h3>
+					{recipe.ingredients.split('\n').map((el, i) => {
+						// console.log(el)
+						return <p key={i}>{el}</p>;
+					})}
 
-          <h3>Zubereitung</h3>
-          {recipe.instructions.split("\n").map((el, i) => {
-            return (
-              <div
-                key={i}
-                dangerouslySetInnerHTML={{ __html: el + "<br/>" }}
-              ></div>
-            );
-          })}
-          {adminLogin && <Button
-            onClick={handleDelete}
-            color="error"
-            variant="contained"
-            style={{ marginTop: "1em" }}
-            fullWidth
-          >
-            Delete
-          </Button>}
-        </Container>
-      ) : (
-        "loading..."
-      )}
-    </>
-  );
+					<h3>Zubereitung</h3>
+					{recipe.instructions.split('\n').map((el, i) => {
+						return <div key={i} dangerouslySetInnerHTML={{ __html: el + '<br/>' }}></div>;
+					})}
+					{adminLogin && (
+						<Button onClick={handleDelete} color='error' variant='contained' style={{ marginTop: '1em' }} fullWidth>
+							Delete
+						</Button>
+					)}
+				</Container>
+			) : (
+				'loading...'
+			)}
+		</>
+	);
 };
 
 export default Recipes;
